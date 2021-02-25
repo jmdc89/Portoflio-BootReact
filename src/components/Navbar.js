d9ad740e8e6a75d26b17333df9f492f3895b9792
Jose Manuel <jmdcarq89@gmail.com>
2021-02-25 21:51:26 +0100

    nav css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import logo from "../logo.png";
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."/>Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
